aaf030ca48ee8dce6af62ec090210476ded454aa
pedro donoso ramirez <pedro.donoso.r@usach.cl>
2022-12-20 19:07:49 -0300

    cambio nombre

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const fs = require('fs');
 
-fs.readFile('index.html', 'utf-8', (err, contenido) => {
+// fs.readFile('index.html', 'utf-8', (err, contenido) => {
+//     if (err) {
+//         console.log(err);
+//         throw err;
+//     } else {
+//         console.log(contenido);
+//     }
+//     console.log('Mensaje...');
+// });
+
+fs.rename('index.html', 'main.html', (err) => {
     if (err) {
-        console.log(err);
-    } else {
-        console.log(contenido);
+        throw err;
     }
+    console.log('Nombre cambiado exitosamente')
 });
 
