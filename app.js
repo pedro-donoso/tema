4289ba2348dc01a1844a5298414bd520e5613af4
pedro donoso ramirez <pedro.donoso.r@usach.cl>
2022-12-20 18:56:16 -0300

    error

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 const fs = require('fs');
 
-fs.readFile()
+fs.readFile('index.html', 'utf-8', (err, contenido) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(contenido);
+    }
+});
+
