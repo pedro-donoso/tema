748d2a4b5353d58e7a4674bb4a7fc91923c5a551
pedro donoso ramirez <pedro.donoso.r@usach.cl>
2022-12-27 14:40:57 -0300

    reemplazo

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 
+// leer un archivo
 // fs.readFile('index.html', 'utf-8', (err, contenido) => {
 //     if (err) {
 //         console.log(err);
@@ -10,10 +11,27 @@ const fs = require('fs');
 //     console.log('Mensaje...');
 // });
 
-fs.rename('index.html', 'main.html', (err) => {
-    if (err) {
-        throw err;
-    }
-    console.log('Nombre cambiado exitosamente')
-});
+// renombrar un archivo
+// fs.rename('index.html', 'main.html', (err) => {
+//     if (err) {
+//         throw err;
+//     }
+//     console.log('Nombre cambiado exitosamente')
+// });
+
+// agregar contenido al final de un archivo
+// fs.appendFile('index.html', '<p>Hola<p/>', (err) => {
+//     if (err) {
+//         throw err;
+//     }
+//     console.log('Archivo actualizado');
+// });
+
+// reemplazar todo el contenido del archivo
+// fs.writeFile('index.html', 'Contenido Nuevo', (err) => {
+//     if (err) {
+//         throw err;
+//     }
+//     console.log('Contenido reemplazado exitosamente');
+// });
 
