7f839f6a759da5bd9a616235d6191c6934fe9eef
pedro donoso ramirez <pedro.donoso.r@usach.cl>
2022-12-27 15:16:39 -0300

    nombre procesos

--- app.js
+++ app.js
@@ -1,39 +1,50 @@
 const fs = require('fs');
 
 // leer un archivo
-// fs.readFile('index.html', 'utf-8', (err, contenido) => {
-//     if (err) {
-//         console.log(err);
-//         throw err;
-//     } else {
-//         console.log(contenido);
-//     }
-//     console.log('Mensaje...');
-// });
 
-// renombrar un archivo
-// fs.rename('index.html', 'main.html', (err) => {
-//     if (err) {
-//         throw err;
-//     }
-//     console.log('Nombre cambiado exitosamente')
-// });
+console.log('Antes de leer el archivo...');
+
+fs.readFile('index.html', 'utf-8', (err, contenido) => {
+     if (err) {
+         console.log(err);
+         throw err;
+     } else {
+         console.log(contenido);
+     }
+     console.log('Mensaje...');
+});
+ 
+console.log('Despues de leer el archivo...');
+
+// Cambiar el nombre de un archivo
+fs.rename('index.html', 'main.html', (err) => {
+     if (err) {
+         throw err;
+    }
+     console.log('Nombre cambiado exitosamente')
+});
+
+console.log('Despues de cambiar el nombre del archivo...');
 
 // agregar contenido al final de un archivo
-// fs.appendFile('index.html', '<p>Hola<p/>', (err) => {
-//     if (err) {
-//         throw err;
-//     }
-//     console.log('Archivo actualizado');
-// });
+fs.appendFile('index.html', '<p>Hola<p/>', (err) => {
+if (err) {
+       throw err;
+    }
+    console.log('Archivo actualizado');
+});
+
+console.log('Despues de agregar contenido al archivo...');
 
 // reemplazar todo el contenido del archivo
-// fs.writeFile('index.html', 'Contenido Nuevo', (err) => {
-//     if (err) {
-//         throw err;
-//     }
-//     console.log('Contenido reemplazado exitosamente');
-// });
+fs.writeFile('index.html', 'Contenido Nuevo', (err) => {
+     if (err) {
+         throw err;
+     }
+     console.log('Contenido reemplazado exitosamente');
+});
+ 
+console.log('Despues de reemplazar el contenido del archivo...');
 
 fs.unlink('main.html', (err) => {
     if (err) {
@@ -41,3 +52,5 @@ fs.unlink('main.html', (err) => {
     }
     console.log('Archivo eliminado');
 });
+
+console.log('Despues de eliminar el archivo...');
